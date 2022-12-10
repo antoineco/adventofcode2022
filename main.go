0951cb81bb10b3db95595007c2e899794454da1a
Antoine Cotten <hello@acotten.com>
2022-12-10 04:14:48 +0100

    Day 03

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 
 	"adventofcode2022/day01"
 	"adventofcode2022/day02"
+	"adventofcode2022/day03"
 	"adventofcode2022/fs"
 )
 
@@ -16,4 +17,7 @@ func main() {
 
 	fmt.Println("Day 02 / Puzzle 1:", day02.Puzzle1(fs, "inputs/d02.txt"))
 	fmt.Println("Day 02 / Puzzle 2:", day02.Puzzle2(fs, "inputs/d02.txt"))
+
+	fmt.Println("Day 03 / Puzzle 1:", day03.Puzzle1(fs, "inputs/d03.txt"))
+	fmt.Println("Day 03 / Puzzle 2:", day03.Puzzle2(fs, "inputs/d03.txt"))
 }
